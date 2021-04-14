c77c2f22548275872f2024bc997732532e616e17
Martin <office@martinpagels.de>
2021-04-14 22:57:33 +0200

    Clean-up react setup
    - Remove unwanted files
    - Add styled-components

---
+++ src/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    background-color: #ffffff;
+    color: #000000;
+}
+`
