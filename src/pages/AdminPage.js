4f4c0bb7f7f9aa155929d5f0e0427a56f26f03ba
Martin <office@martinpagels.de>
2021-05-25 22:47:56 +0200

    Fix key error

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -5,6 +5,8 @@ import ItemContext from '../context/ItemContext'
 export default function AdminPage() {
   const { items } = useContext(ItemContext)
   return items
-    ? items.map((item, index) => <ItemForAdmin key={item.id} data={item} />)
+    ? items.map((item, index) => (
+        <ItemForAdmin key={`${item.id}_${index}`} data={item} />
+      ))
     : ''
 }
