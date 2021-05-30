fc23fa24037cb78acb60a1b3c36c7079b519cef9
Martin <office@martinpagels.de>
2021-05-30 23:09:13 +0200

    Refactor sorting Items
    - Add getOrderedItemList function
    - Abstract Code in userPage

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -3,47 +3,13 @@ import { useContext } from 'react'
 import ItemContext from '../context/ItemContext'
 import UserContext from '../context/UserContext'
 import Item from '../components/Item'
+import getOrderedItemList from '../utils/getOrderedItemList'
 
 export default function UserPage() {
   const { items, saveNewItem } = useContext(ItemContext)
   const { user } = useContext(UserContext)
 
-  const sortedItems = items?.sort((a, b) => {
-    const itemA =
-      a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
-    console.log('itemA', itemA)
-    const itemB =
-      b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
-    return itemA - itemB
-  })
-
-  const listOfItemsWithDonationFullfiled = []
-  const listOfItemsWithDontationNeeded = []
-
-  sortedItems?.forEach((item) => {
-    if (
-      item.donations.reduce((pre, cur) => pre + cur.amount, 0) === item.price
-    ) {
-      listOfItemsWithDonationFullfiled.push(item)
-    } else {
-      listOfItemsWithDontationNeeded.push(item)
-    }
-  })
-
-  const sortedListOfItemsWithDontationNeeded =
-    listOfItemsWithDontationNeeded.sort((a, b) => {
-      const itemA =
-        a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
-      console.log('itemA', itemA)
-      const itemB =
-        b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
-      return itemA - itemB || a.price - b.price
-    })
-
-  const itemsToRender = [
-    ...sortedListOfItemsWithDontationNeeded,
-    ...listOfItemsWithDonationFullfiled,
-  ]
+  const itemsToRender = getOrderedItemList(items)
 
   return (
     <Main>
---
+++ src/utils/getOrderedItemList.js
@@ -0,0 +1,38 @@
+export default function getOrderedItemList(items) {
+  const sortedItems = items?.sort((a, b) => {
+    const itemA =
+      a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
+    console.log('itemA', itemA)
+    const itemB =
+      b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
+    return itemA - itemB
+  })
+
+  const listOfItemsWithDonationFullfiled = []
+  const listOfItemsWithDontationNeeded = []
+
+  sortedItems?.forEach((item) => {
+    if (
+      item.donations.reduce((pre, cur) => pre + cur.amount, 0) === item.price
+    ) {
+      listOfItemsWithDonationFullfiled.push(item)
+    } else {
+      listOfItemsWithDontationNeeded.push(item)
+    }
+  })
+
+  const sortedListOfItemsWithDontationNeeded =
+    listOfItemsWithDontationNeeded.sort((a, b) => {
+      const itemA =
+        a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
+      console.log('itemA', itemA)
+      const itemB =
+        b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
+      return itemA - itemB || a.price - b.price
+    })
+
+  return [
+    ...sortedListOfItemsWithDontationNeeded,
+    ...listOfItemsWithDonationFullfiled,
+  ]
+}
