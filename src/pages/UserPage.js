12415fa0898b3265fa3995dc063308117d0cede3
Martin <office@martinpagels.de>
2021-05-30 18:51:01 +0200

    WIP sort Item function

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -8,10 +8,47 @@ export default function UserPage() {
   const { items, saveNewItem } = useContext(ItemContext)
   const { user } = useContext(UserContext)
 
+  const sortedItems = items?.sort((a, b) => {
+    const itemA =
+      a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
+    console.log('itemA', itemA)
+    const itemB =
+      b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
+    return itemA - itemB
+  })
+
+  const listOfItemsWithDonationFullfiled = []
+  const listOfItemsWithDontationNeeded = []
+
+  sortedItems?.forEach((item) => {
+    if (
+      item.donations.reduce((pre, cur) => pre + cur.amount, 0) === item.price
+    ) {
+      listOfItemsWithDonationFullfiled.push(item)
+    } else {
+      listOfItemsWithDontationNeeded.push(item)
+    }
+  })
+
+  const sortedListOfItemsWithDontationNeeded =
+    listOfItemsWithDontationNeeded.sort((a, b) => {
+      const itemA =
+        a.price - a.donations.reduce((pre, cur) => pre + cur.amount, 0)
+      console.log('itemA', itemA)
+      const itemB =
+        b.price - b.donations.reduce((pre, cur) => pre + cur.amount, 0)
+      return itemA - itemB || a.price - b.price
+    })
+
+  const itemsToRender = [
+    ...sortedListOfItemsWithDontationNeeded,
+    ...listOfItemsWithDonationFullfiled,
+  ]
+
   return (
     <Main>
       {items &&
-        items.map((item, index) => (
+        itemsToRender.map((item, index) => (
           <Item
             key={`${item.id}_${index}`}
             data={item}
